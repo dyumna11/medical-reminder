a992e40ecff473ba9e5d987695b5b4ed007a140a
Dyumna <dyumna.negi1@gmail.com>
2026-06-13 00:25:50 +0530

    Fix Node version and dependencies

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 const reminderRoutes = require("./routes/reminderRoutes");
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 // Routes
