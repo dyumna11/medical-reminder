fbec35158453d97d7dcd483d37d3007d56b94380
Dyumna <dyumna.negi1@gmail.com>
2026-06-14 23:56:05 +0530

    Changed service

--- server/sendEmail.js
+++ server/sendEmail.js
@@ -6,7 +6,9 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
